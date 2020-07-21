40029ece94bc7144afedc71391769acba730f2cd
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-07-21 11:51:39 -0300

    Identifying Classes

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using ACM.BL;
+using NUnit.Framework;
+
+namespace ACM.BLTest
+{
+    [TestFixture]
+    public class CustomerTest
+    {
+        [Test]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+            
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void FullNameFistNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+            string expected = "Baggins";
+
+            //-- Act
+            string actual = customer.FullName;
+            
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+            string expected = "Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+            
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void StaticTest()
+        {
+            //-- Arrange
+            Customer c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount += 1;
+
+            Customer c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            Customer c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            //-- Act
+            
+            //-- Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [Test]
+        public void ValidateValid()
+        {
+            var customer = new Customer();
+            
+        }
+    }
+}
